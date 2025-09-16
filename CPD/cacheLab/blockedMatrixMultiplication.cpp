5fd49e6c0316d6847129f84b7fcd25a522cca1b8
sumire25 <msumirer@unsa.edu.pe>
2025-09-16 09:03:42 -0500

    corrected code

--- CPD/cacheLab/blockedMatrixMultiplication.cpp
+++ CPD/cacheLab/blockedMatrixMultiplication.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <random>
+
 #define MSIZE 1000
 #define BSIZE 100
 
-int main(){
-  double A[MSIZE][MSIZE], B[MSIZE][MSIZE], C[MSIZE][MSIZE] = {0};
-  for (int blockI = 0; blockI < MSIZE; blockI += BSIZE)
-    for (int blockJ = 0; blockJ < MSIZE; blockJ += BSIZE)
-      for (int blockK = 0; blockK < MSIZE; blockK += BSIZE)
-        for (int i = blockI; i < blockI + BSIZE; i++)
-          for (int j = blockJ; j < blockJ + BSIZE; j++)
-            for (int k = blockK; k < blockK + BSIZE; k++)
-              C[i][j] += A[i][k] * B[k][j];
+#define VVD std::vector<std::vector<double>>
 
-  return 0;
+void blockedMatrixMultiplication(VVD& A, VVD& B, VVD& C) {
+    for (int blockI = 0; blockI < MSIZE; blockI += BSIZE) {
+        for (int blockJ = 0; blockJ < MSIZE; blockJ += BSIZE) {
+            for (int blockK = 0; blockK < MSIZE; blockK += BSIZE) {
+                for (int i = blockI; i < blockI + BSIZE; i++) {
+                    for (int j = blockJ; j < blockJ + BSIZE; j++) {
+                        for (int k = blockK; k < blockK + BSIZE; k++) {
+                            C[i][j] += A[i][k] * B[k][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    VVD A(MSIZE, std::vector<double>(MSIZE));
+    VVD B(MSIZE, std::vector<double>(MSIZE));
+    VVD C(MSIZE, std::vector<double>(MSIZE, 0));
+    blockedMatrixMultiplication(A, B, C);
+
+    return 0;
 }
--- CPD/cacheLab/classicMatrixMultiplication.cpp
+++ CPD/cacheLab/classicMatrixMultiplication.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <random>
+
 #define MSIZE 1000
+#define VVD std::vector<std::vector<double>>
 
-int main(){
-  double A[MSIZE][MSIZE], B[MSIZE][MSIZE], C[MSIZE][MSIZE] = {0};
-  for (int i = 0; i < MSIZE; i++)
-    for (int j = 0; j < MSIZE; j++)
-      for (int k = 0; k < MSIZE; k++)
-        C[i][j] += A[i][k] * B[k][j];
+void classicMatrixMultiplication(VVD& A, VVD& B, VVD& C) {
+    for (int i = 0; i < MSIZE; i++)
+        for (int j = 0; j < MSIZE; j++)
+            for (int k = 0; k < MSIZE; k++)
+                C[i][j] += A[i][k] * B[k][j];
+}
 
-  return 0;
+int main() {
+    VVD A(MSIZE, std::vector<double>(MSIZE));
+    VVD B(MSIZE, std::vector<double>(MSIZE));
+    VVD C(MSIZE, std::vector<double>(MSIZE, 0));
+
+    classicMatrixMultiplication(A, B, C);
+
+    return 0;
 }
